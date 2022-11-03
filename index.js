5e0edde8c8ce2f344daa80ea35de46a25317ac18
CodeBuilder94 <113739521+CodeBuilder94@users.noreply.github.com>
2022-11-03 15:59:56 -0700

    Getting the snake moving pt1
    started on the process of getting the snake up update it's position. Have not added more sections yet.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //get game board
 let board = document.querySelector("#gameBoard");
+let head =null;
 
 let snake = {
-    body:[],
+    body:[], //array of arrays
     nextDirection:[1,0] //determins the direction of the snake (x,y)
 }
 
@@ -19,26 +20,48 @@ function startGame(ev)
     ev.target.remove();
 
     //create the snake
-    createSnake();
+   let head = createSnake();
+    
+    //add head location to snake obj
+    headStart();
 }
 
 //create the snake
 function createSnake()
 {
-    let head = document.createElement("div");
+    head = document.createElement("div");
     head.style.height = "30px";
     head.style.width = "30px";
     head.style.backgroundColor ="green";
-    board.appendChild(head);
-
-    //add head location to snake obj
-    headStart();
+    board.appendChild(head); 
+    return head;
 }
 
 function headStart()
 {
-    snake.body.push();
+    //get the location data of the head
+    let headLocation =head.getBoundingClientRect();
+    snake.body.push([]);
+    snake.body[0].push(headLocation.x); //the x
+    snake.body[0].push(headLocation.y); //the y
+    console.log(snake.body);
+
 }
+
+//have the game update
+function tick()
+{
+    
+
+    //change the x
+    snake.body[0][0]= snake.body[0][0]+ snake.nextDirection[0];
+    
+    //change the y
+    snake.body[0][1] =snake.body[0][1] + snake.nextDirection[1];
+}
+
+//update the game every 30 frames
+setInterval(tick, 1000/30)
 
 //start button pressed
 let startButton = document.querySelector("#start");
