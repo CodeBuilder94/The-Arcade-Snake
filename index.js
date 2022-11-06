c528108759f583a39a3e3645370684e17b91a2ca
CodeBuilder94 <113739521+CodeBuilder94@users.noreply.github.com>
2022-11-06 16:38:23 -0600

    moving snake pt5
    got the snake head to move up but still need work on moving left and right.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let head =null;
 
 let snake = {
     body:[[]], //array of arrays
-    nextDirection:[1,0] //determins the direction of the snake (x,y)
+    nextDirection:[0,-1] //determins the direction of the snake (x,y)
 }
 
 let gameState ={
@@ -37,6 +37,11 @@ function createSnake()
     head.style.width = "30px";
     head.style.backgroundColor ="green";
     head.style.position ="absolute";
+    head.style.top ="300px";
+    head.style.left ="300px";
+    head.style.right = "300px";
+    head.style.bottom = "300px";
+   
     board.appendChild(head); 
     return head;
 }
@@ -49,9 +54,6 @@ function headStart()
     let snakeBodyArr = snake.body;
     snakeBodyArr[0].push(headLocation.x); //the x
     snakeBodyArr[0].push(headLocation.y); //the y
-    
-    
-
 }
 
 //have the game update
@@ -68,25 +70,88 @@ function tick()
     snakeArr[1] =snakeArr[1] + 10
 
     //go and render the movement of the snake
-    render(snakeArr);
+    moveHead();
+    //render(snakeArr);
+
+
+}
+
+function moveHead()
+{
+    if(snake.nextDirection[1]===-1)
+    {
+        //move up
+        head.style.transition = "top 10s linear";
+        head.style.top = "-10px";
+        
+        
+    }
+    else if(snake.nextDirection[1] === 1)
+    {
+        //move down
+        head.style.transition = "bottom 10s linear";
+        head.style.bottom = "10px";
+        
+    }
+    else if(snake.nextDirection[0]===1)
+    {
+        //move left
+        head.style.transition = "left 10s linear";
+        head.style.left = "10px";
+    }
+    else
+    {
+        head.style.transition = "right 10s linear";
+        head.style.right = "10px";
+    }
 }
     
-function render(snakeArr)
-{
+//function render(snakeArr)
+//{
     //let headLocation = head.getBoundingClientRect();
     //headLocation.x 
     
     //move the head up
-    
-    head.style.transform = "translateY(- 10px)";
+    //head.style.transform = "translateY(-10px)";
     
     
     //headLocation.y = snakeArr[1];
     //console.log(headLocation.y);
-}
+//}
 
 
 
 //start button pressed
 let startButton = document.querySelector("#start");
 startButton.addEventListener("click",startGame);
+
+//arrow keys
+document.onkeyup = function(ev)
+{
+    switch (ev.keyCode)
+    {
+        case 37:
+            //left arrow
+            snake.nextDirection[0]=1;
+            snake.nextDirection[1]=0;
+            head.style.transition ="notransition";
+            break;
+        case 38:
+            //up arrow
+            snake.nextDirection[0]=0;
+            snake.nextDirection[1]=-1;
+            head.style.transition ="notransition";
+            break;
+        case 39:
+            //right arrow
+            snake.nextDirection[0]=-1;
+            snake.nextDirection[1]=0;
+            head.style.transition ="notransition";
+            break;
+        case 40:
+            //down arrow
+            snake.nextDirection[0]=0;
+            snake.nextDirection[1]=1;
+            head.style.transition ="notransition";
+    }
+}
